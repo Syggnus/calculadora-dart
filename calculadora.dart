5dda77002107c5422fb3b049e34b79352ed0088c
Chuan Igor <38382865+Syggnus@users.noreply.github.com>
2022-07-12 13:09:35 -0300

    Refactor
    Refatorando o código:
    Mais limpo e legível
    Obs: (Na validação na linha 17 ainda falta restringir o uso de qualquer outro caracteres sem ser número)

--- calculadora.dart
+++ calculadora.dart
@@ -1,70 +1,57 @@
 import 'dart:io';
 
 void main() {
-  print("Bem vindo a CalcDart 1.0");
+  print("Bem vindo a CalcDart 2.0");
   calculadora();
 }
 
 calculadora() {
   print(
-      "Escolha abaixo a operação que deseja: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
+      "Escolha abaixo a operação que deseja:\n[1] - Adição\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão");
+  print("Em seguida escolha dois números para realizar a operação");
+
   String? escolha = stdin.readLineSync();
+  String? num1 = stdin.readLineSync();
+  String? num2 = stdin.readLineSync();
 
-  switch (escolha) {
-    case "1":
-      print("Escolha os números para realizar a Adição");
-      String? num_add1 = stdin.readLineSync();
-      String? num_add2 = stdin.readLineSync();
-
-      if (num_add1 != null && num_add2 != null) {
-        double resultado_add = double.parse(num_add1) + double.parse(num_add2);
-        print("O resultado é: $resultado_add");
-      }
-      break;
-    case "2":
-      print("Escolha os números para realizar a Subtração");
-      String? num_sub1 = stdin.readLineSync();
-      String? num_sub2 = stdin.readLineSync();
-
-      if (num_sub1 != null && num_sub2 != null) {
-        double resultado_sub = double.parse(num_sub1) - double.parse(num_sub2);
-        print("O resultado é: $resultado_sub");
+  if (num1 != null && num2 != null) {
+    switch (escolha) {
+      case "1":
+        double resultado = double.parse(num1) + double.parse(num2);
+        print("O Resultado da Adição é: $resultado");
         break;
-      }
-      break;
-
-    case "3":
-      print("Escolha os números para realizar a Multiplicação");
-      String? num_mut1 = stdin.readLineSync();
-      String? num_mut2 = stdin.readLineSync();
-
-      if (num_mut1 != null && num_mut2 != null) {
-        double resultado_mut = double.parse(num_mut1) * double.parse(num_mut2);
-        print("O resultado é: $resultado_mut");
+      case "2":
+        double resultado = double.parse(num1) - double.parse(num2);
+        print("O Resultado da Subtração é: $resultado");
         break;
-      }
-      break;
-
-    case "4":
-      print("Escolha os números para realizar a Divisão");
-      String? num_div1 = stdin.readLineSync();
-      String? num_div2 = stdin.readLineSync();
-
-      if (num_div1 != null && num_div2 != null) {
-        double resultado_div = double.parse(num_div1) / double.parse(num_div2);
-        print("O resultado é: $resultado_div");
+      case "3":
+        double resultado = double.parse(num1) * double.parse(num2);
+        print("O Resultado da Multiplicação é: $resultado");
         break;
-      }
-      break;
+      case "4":
+        double resultado = double.parse(num1) / double.parse(num2);
+        print("O Resultado da Divisão é: $resultado");
+        break;
+      default:
+        print("Escolha uma opção válida!");
+        calculadora();
+    }
   }
-  print("Fim da Operação\nDeseja realizar outra operação ?\n1 - Sim\n2 - Não");
+  novocalculo();
+}
+
+novocalculo() {
+  print("Deseja realizar outra operação ?\n1 - Sim\n2 - Não");
   String? escolha2 = stdin.readLineSync();
   switch (escolha2) {
     case "1":
       calculadora();
       break;
     case "2":
-      print("Byee!");
-      return;
+      print("Operação Finalizada.");
+      break;
+    default:
+      print("Opção inválida");
+      novocalculo();
   }
 }
